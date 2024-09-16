6125ecc1cd015506b1b599f05d6990e7eac0824e
ojochau <ojochau@instajs.com>
2024-09-16 17:18:51 +0800

    Fix get product info issue

--- dist/index.js
+++ dist/index.js
@@ -29,44 +29,42 @@ define("@scom/scom-ton-subscription/model.ts", ["require", "exports", "@ijstech/
             this.rpcWalletId = '';
             this.networkMap = {};
             this.infuraId = 'adc596bf88b648e2a8902bc9093930c5';
-            this.contractInfoByChain = [
-                {
-                    "97": {
-                        "ProductMarketplace": {
-                            "address": "0x93e684ad2AEE178e23675fbE5bA88c3e4e7467f4"
-                        },
-                        "OneTimePurchaseNFT": {
-                            "address": "0x5aE9c7f08572D52e2DB8508B502D767A1ECf21Bf"
-                        },
-                        "SubscriptionNFTFactory": {
-                            "address": "0x0055e4edb49425A29784Bd9a7986F5b56dcc8f6b"
-                        },
-                        "Promotion": {
-                            "address": "0x13d23201a8A6661881d701E1cF56A30A8eb0aE90"
-                        },
-                        "Commission": {
-                            "address": "0xcdc39C8bC8F9fDAF31D79f461B47477606770c62"
-                        }
+            this.contractInfoByChain = {
+                "97": {
+                    "ProductMarketplace": {
+                        "address": "0x93e684ad2AEE178e23675fbE5bA88c3e4e7467f4"
                     },
-                    "43113": {
-                        "ProductMarketplace": {
-                            "address": "0xeC3747eAbf71D4BDF15Abb70398C04B642363D10"
-                        },
-                        "OneTimePurchaseNFT": {
-                            "address": "0x404eeCC44F7aFc1f7561b2A9bC475513206D4b15"
-                        },
-                        "SubscriptionNFTFactory": {
-                            "address": "0x9231761Bd5f32c8f6465d82168BAdaB109D23290"
-                        },
-                        "Promotion": {
-                            "address": "0x22786FF4E595f1B517242549ec1D263e62dc6F26"
-                        },
-                        "Commission": {
-                            "address": "0x2Ed01CB805e7f52c92cfE9eC02E7Dc899cA53BCa"
-                        }
+                    "OneTimePurchaseNFT": {
+                        "address": "0x5aE9c7f08572D52e2DB8508B502D767A1ECf21Bf"
+                    },
+                    "SubscriptionNFTFactory": {
+                        "address": "0x0055e4edb49425A29784Bd9a7986F5b56dcc8f6b"
+                    },
+                    "Promotion": {
+                        "address": "0x13d23201a8A6661881d701E1cF56A30A8eb0aE90"
+                    },
+                    "Commission": {
+                        "address": "0xcdc39C8bC8F9fDAF31D79f461B47477606770c62"
+                    }
+                },
+                "43113": {
+                    "ProductMarketplace": {
+                        "address": "0xeC3747eAbf71D4BDF15Abb70398C04B642363D10"
+                    },
+                    "OneTimePurchaseNFT": {
+                        "address": "0x404eeCC44F7aFc1f7561b2A9bC475513206D4b15"
+                    },
+                    "SubscriptionNFTFactory": {
+                        "address": "0x9231761Bd5f32c8f6465d82168BAdaB109D23290"
+                    },
+                    "Promotion": {
+                        "address": "0x22786FF4E595f1B517242549ec1D263e62dc6F26"
+                    },
+                    "Commission": {
+                        "address": "0x2Ed01CB805e7f52c92cfE9eC02E7Dc899cA53BCa"
                     }
                 }
-            ];
+            };
         }
         get wallets() {
             return [
@@ -138,6 +136,7 @@ define("@scom/scom-ton-subscription/model.ts", ["require", "exports", "@ijstech/
         ;
         async initWallet() {
             await eth_wallet_1.Wallet.getClientInstance().init();
+            await this.getRpcWallet()?.init();
         }
         async connectWallet() { }
         isClientWalletConnected() {
--- src/model.ts
+++ src/model.ts
@@ -11,44 +11,42 @@ export class SubscriptionModel {
     private rpcWalletId: string = '';
     private networkMap = {} as { [key: number]: INetwork };
     private infuraId: string = 'adc596bf88b648e2a8902bc9093930c5';
-    private contractInfoByChain = [
-        {
-            "97": {
-                "ProductMarketplace": {
-                    "address": "0x93e684ad2AEE178e23675fbE5bA88c3e4e7467f4"
-                },
-                "OneTimePurchaseNFT": {
-                    "address": "0x5aE9c7f08572D52e2DB8508B502D767A1ECf21Bf"
-                },
-                "SubscriptionNFTFactory": {
-                    "address": "0x0055e4edb49425A29784Bd9a7986F5b56dcc8f6b"
-                },
-                "Promotion": {
-                    "address": "0x13d23201a8A6661881d701E1cF56A30A8eb0aE90"
-                },
-                "Commission": {
-                    "address": "0xcdc39C8bC8F9fDAF31D79f461B47477606770c62"
-                }
+    private contractInfoByChain = {
+        "97": {
+            "ProductMarketplace": {
+                "address": "0x93e684ad2AEE178e23675fbE5bA88c3e4e7467f4"
             },
-            "43113": {
-                "ProductMarketplace": {
-                    "address": "0xeC3747eAbf71D4BDF15Abb70398C04B642363D10"
-                },
-                "OneTimePurchaseNFT": {
-                    "address": "0x404eeCC44F7aFc1f7561b2A9bC475513206D4b15"
-                },
-                "SubscriptionNFTFactory": {
-                    "address": "0x9231761Bd5f32c8f6465d82168BAdaB109D23290"
-                },
-                "Promotion": {
-                    "address": "0x22786FF4E595f1B517242549ec1D263e62dc6F26"
-                },
-                "Commission": {
-                    "address": "0x2Ed01CB805e7f52c92cfE9eC02E7Dc899cA53BCa"
-                }
+            "OneTimePurchaseNFT": {
+                "address": "0x5aE9c7f08572D52e2DB8508B502D767A1ECf21Bf"
+            },
+            "SubscriptionNFTFactory": {
+                "address": "0x0055e4edb49425A29784Bd9a7986F5b56dcc8f6b"
+            },
+            "Promotion": {
+                "address": "0x13d23201a8A6661881d701E1cF56A30A8eb0aE90"
+            },
+            "Commission": {
+                "address": "0xcdc39C8bC8F9fDAF31D79f461B47477606770c62"
+            }
+        },
+        "43113": {
+            "ProductMarketplace": {
+                "address": "0xeC3747eAbf71D4BDF15Abb70398C04B642363D10"
+            },
+            "OneTimePurchaseNFT": {
+                "address": "0x404eeCC44F7aFc1f7561b2A9bC475513206D4b15"
+            },
+            "SubscriptionNFTFactory": {
+                "address": "0x9231761Bd5f32c8f6465d82168BAdaB109D23290"
+            },
+            "Promotion": {
+                "address": "0x22786FF4E595f1B517242549ec1D263e62dc6F26"
+            },
+            "Commission": {
+                "address": "0x2Ed01CB805e7f52c92cfE9eC02E7Dc899cA53BCa"
             }
         }
-    ];
+    };
 
     get wallets(): IWalletPlugin[] {
         return [
@@ -81,7 +79,7 @@ export class SubscriptionModel {
     }
 
     getRpcWallet() {
-      return this.rpcWalletId ? Wallet.getRpcWalletInstance(this.rpcWalletId) : null;
+        return this.rpcWalletId ? Wallet.getRpcWalletInstance(this.rpcWalletId) : null;
     }
 
     getDefaultData() {
@@ -125,6 +123,7 @@ export class SubscriptionModel {
 
     async initWallet() {
         await Wallet.getClientInstance().init();
+        await this.getRpcWallet()?.init();
     }
 
     async connectWallet() { }
@@ -134,42 +133,42 @@ export class SubscriptionModel {
     }
 
     initRpcWallet(defaultChainId: number) {
-      if (this.rpcWalletId) {
-        return this.rpcWalletId;
-      }
-      const clientWallet = Wallet.getClientInstance();
-      const networkList: INetwork[] = Object.values(application.store?.networkMap || []);
-      const instanceId = clientWallet.initRpcWallet({
-        networks: networkList,
-        defaultChainId,
-        infuraId: application.store?.infuraId,
-        multicalls: application.store?.multicalls
-      });
-      this.rpcWalletId = instanceId;
-      if (clientWallet.address) {
-        const rpcWallet = Wallet.getRpcWalletInstance(instanceId);
-        rpcWallet.address = clientWallet.address;
-      }
-  
-      const defaultNetworkList = getNetworkList();
-      const defaultNetworkMap = defaultNetworkList.reduce((acc, cur) => {
-        acc[cur.chainId] = cur;
-        return acc;
-      }, {});
-      for (let network of networkList) {
-        const networkInfo = defaultNetworkMap[network.chainId];
-        if (!networkInfo) continue;
-        if (this.infuraId && network.rpcUrls && network.rpcUrls.length > 0) {
-          for (let i = 0; i < network.rpcUrls.length; i++) {
-            network.rpcUrls[i] = network.rpcUrls[i].replace(/{InfuraId}/g, this.infuraId);
-          }
+        if (this.rpcWalletId) {
+            return this.rpcWalletId;
         }
-        this.networkMap[network.chainId] = {
-          ...networkInfo,
-          ...network
-        };
-      }
-      return instanceId;
+        const clientWallet = Wallet.getClientInstance();
+        const networkList: INetwork[] = Object.values(application.store?.networkMap || []);
+        const instanceId = clientWallet.initRpcWallet({
+            networks: networkList,
+            defaultChainId,
+            infuraId: application.store?.infuraId,
+            multicalls: application.store?.multicalls
+        });
+        this.rpcWalletId = instanceId;
+        if (clientWallet.address) {
+            const rpcWallet = Wallet.getRpcWalletInstance(instanceId);
+            rpcWallet.address = clientWallet.address;
+        }
+
+        const defaultNetworkList = getNetworkList();
+        const defaultNetworkMap = defaultNetworkList.reduce((acc, cur) => {
+            acc[cur.chainId] = cur;
+            return acc;
+        }, {});
+        for (let network of networkList) {
+            const networkInfo = defaultNetworkMap[network.chainId];
+            if (!networkInfo) continue;
+            if (this.infuraId && network.rpcUrls && network.rpcUrls.length > 0) {
+                for (let i = 0; i < network.rpcUrls.length; i++) {
+                    network.rpcUrls[i] = network.rpcUrls[i].replace(/{InfuraId}/g, this.infuraId);
+                }
+            }
+            this.networkMap[network.chainId] = {
+                ...networkInfo,
+                ...network
+            };
+        }
+        return instanceId;
     }
 
     async getTokenInfo(address: string, chainId: number) {
